68e306b994525ac2e2c91d801ea59a892ab4c185
DarthNihilus1 <mgracer48@yahoo.com>
2023-07-05 04:42:26 -0400

    attempt to fix windows and possibly linux build

--- compile_scripts/compile-linux.sh
+++ compile_scripts/compile-linux.sh
@@ -5,11 +5,12 @@ sudo apt-get install -y python3-pip
 # install qt6
 sudo apt-get install build-essential libgl1-mesa-dev
 
-sudo apt-get install -y qt6-base-dev
 sudo apt-get install -y python3-pyqt6
 cd ..
+
+# install requirements 
 python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt pyinstaller pillow
-pyinstaller --onefile --clean --add-data "resources/*.png:resources/"  main.py --name "Toontown Rewritten Custom Launcher" -i "eyes.icns"
+python3 -m pip install -r requirements.txt pyinstaller>=5.12 pyinstaller-hooks-contrib==2023.4 pillow
+pyinstaller  --onefile --clean --noconsole --noupx --add-data "resources/*.png:resources/" --icon=eyes.icns --name="Toontown Rewritten Custom Launcher" main.py
 
 
--- compile_scripts/compile.bat
+++ compile_scripts/compile.bat
@@ -1,5 +1,12 @@
+# some things modified based on https://github.com/MCMi460/NSO-RPC/blob/main/scripts/build.bat
 @echo off
 cd ..
-pyinstaller --onefile main.py --clean --noconsole --add-data "resources/*.png;resources/" -n "Toontown Rewritten Custom Launcher.exe" -i "eyes.icns"
-rmdir build /s /q
+@echo off
+
+
+REM Install requirements
+python -m pip install -r ./requirements.txt pypiwin32 winshell pyinstaller>=5.12 pyinstaller-hooks-contrib==2023.4 pillow 
+
+REM Build the executable using PyInstaller
+python -m PyInstaller --onefile --clean --noconsole --noupx --add-data "resources/*.png;resources/" --icon=eyes.icns --name="Toontown Rewritten Custom Launcher" main.py
 
